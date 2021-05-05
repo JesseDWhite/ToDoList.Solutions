6afc003992f596d69b3465ede5d3176d5b95b32b
Jesse White <jesse.white6@gmail.com>
2021-05-04 20:37:51 -0700

    remove type error with one error still remaining.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    private static ToDoList<Item> _instances = new List<Item> { };
+    private static List<Item> _instances = new List<Item> { };
     public static List<Item> GetAll()
     {
       return _instances;
