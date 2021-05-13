c818f9abefb86a172b48a85ee477be750fcb4ef5
Jesse White <jesse.white6@gmail.com>
2021-05-12 23:02:17 -0700

    add CRUD and RESTful functionallity.

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -6,29 +6,31 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
-    [HttpPost("/items")]
 
-    public ActionResult Create(string description)
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      Item myItem = new Item(description);
-      return View(RedirectToAction("Index"));
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
+
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+
     [HttpPost("/items/delete")]
     public ActionResult DeleteAll()
     {
       Item.ClearAll();
       return View();
     }
+
   }
 }
